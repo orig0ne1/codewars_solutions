bb2768123b4d5d7343bcccd91d5c480298f93397
orig0ne <drekmark787@gmail.com>
2025-08-23 02:45:57 +0300

    complete

--- src/java/DisemvowelTrolls.java
+++ src/java/DisemvowelTrolls.java
@@ -1,3 +1,35 @@
-public class DisemvowelTrolls {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
+public class DisemvowelTrolls {
+    static char[] vowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите что-нибудь:");
+        String input = reader.readLine();
+        System.out.println(sayNoToTrolls(input));
+    }
+
+    public static String sayNoToTrolls(String comment){
+        StringBuilder sb = new StringBuilder();
+        List<Character> charList = comment.chars()
+                .mapToObj(c -> (char) c)
+                .toList();
+        for (Character c : charList) {
+            if (isVowel(c)) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static boolean isVowel(char c) {
+        for(char vowel : vowels) {
+            if (vowel == c) return true;
+        }
+        return false;
+    }
 }
