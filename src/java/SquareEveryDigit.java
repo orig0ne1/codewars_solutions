61dd046f5e17ad042222c1c17a6d395e0e072c77
orig0ne <drekmark787@gmail.com>
2025-08-23 03:02:14 +0300

    Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
    For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
    
    Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
    
    Note: The function accepts an integer and returns an integer.
    
    Happy Coding!

---
+++ src/java/SquareEveryDigit.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SquareEveryDigit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        System.out.println(squareEveryDigit(str));
+    }
+
+    public static String squareEveryDigit(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (String element : str.split("")) {
+            sb.append(square(element));
+        }
+        return sb.toString();
+    }
+
+    public static int square(String num) {
+        return Integer.parseInt(num) ^ 2;
+    }
+
+}
