4cf977f83b774d0639c21fcc764aa1523cd42c8d
orig0ne <drekmark787@gmail.com>
2025-08-23 03:02:56 +0300

    complete

--- src/java/SquareEveryDigit.java
+++ src/java/SquareEveryDigit.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class SquareEveryDigit {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
