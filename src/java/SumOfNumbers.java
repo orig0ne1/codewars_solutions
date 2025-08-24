dfd0190cb74d9926697198fceedf6b8a31126013
orig0ne <drekmark787@gmail.com>
2025-08-24 17:49:29 +0300

    complete

--- src/java/SumOfNumbers.java
+++ src/java/SumOfNumbers.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int first = scan.nextInt();
+        int last = scan.nextInt();
+        System.out.println("Res: " + sumOfNumbers(first, last));
+    }
+
+    public static int sumOfNumbers(int first, int last) {
+        int sum = 0;
+        for(int i = first; i <= last; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
