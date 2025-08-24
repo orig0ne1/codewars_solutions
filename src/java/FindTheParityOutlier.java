5ed30d44b3c7e8328b46272a542faee8c361009e
orig0ne <drekmark787@gmail.com>
2025-08-24 17:41:00 +0300

    complete

--- src/java/FindTheParityOutlier.java
+++ src/java/FindTheParityOutlier.java
@@ -1,3 +1,29 @@
-public class FindTheParityOutlier {
+import java.util.Scanner;
 
+public class FindTheParityOutlier {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(getOutlier(arr));
+    }
+
+    public static int getOutlier(int[] arr) {
+        int firstThreeEvenCount = 0;
+        for (int i = 0; i < Math.min(3, arr.length); i++) {
+            if (arr[i] % 2 == 0) {
+                firstThreeEvenCount++;
+            }
+        }
+        boolean mostAreEven = firstThreeEvenCount >= 2;
+        for (int j : arr) {
+            if ((j % 2 == 0) != mostAreEven) {
+                return j;
+            }
+        }
+        return 0;
+    }
 }
